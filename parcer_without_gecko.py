3d3661d8c201bd38dad967203b66e16cef94627d
interuser <kurochkin-va@yandex.ru>
2020-04-07 13:32:26 +0300

    7_4_20_release

--- parcer_without_gecko.py
+++ parcer_without_gecko.py
@@ -7,6 +7,10 @@ import threading
 import os
 import http.server
 
+from requests import session
+import json
+from pprint import pprint
+
 def weathercheck():
     UserAgent().chrome
     url = 'https://www.gismeteo.ru/weather-sankt-peterburg-4079/'
@@ -25,6 +29,33 @@ def coincheck():
     coin_first = (coin[0].text)
     return (coin_first.strip())
 
+def petrolcheck():
+    my_session = session()
+
+    my_session.headers['Host'] = 'www.rosneft-azs.ru'
+    my_session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0'
+    my_session.headers['Accept'] = 'application/json, text/javascript, */*; q=0.01'
+    my_session.headers['Accept-Language'] = 'ru-RU'
+    my_session.headers['Accept-Encoding'] = 'gzip, deflate'
+    my_session.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+    my_session.headers['X-Requested-With'] = 'XMLHttpRequest'
+    my_session.headers['Content-Length'] = '9'
+    my_session.headers['Origin'] = 'http://www.rosneft-azs.ru'
+    my_session.headers['Connection'] = 'keep-alive'
+    my_session.headers['Referer'] = 'http://www.rosneft-azs.ru/fuel_search'
+    my_session.headers['Pragma'] = 'no-cache'
+    my_session.headers['Cache-Control'] = 'no-cache'
+
+    # print(my_session.headers)
+    all_request = my_session.post('http://www.rosneft-azs.ru/fuel_search', data={'region': "78"})
+    # print(all_request.text)
+    all_rec_string = json.loads(all_request.text)
+    for elem in all_rec_string['price']:
+        if elem['type'] == '95':
+            extract = elem['avg']
+            return (int(float(extract) * 100) / 100)
+
+
 # проверяем парсер
 print("Парсер погоды отработал и возвращает " + weathercheck())
 
@@ -36,6 +67,7 @@ def get_text_messages(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, питушара. В Питере " + weathercheck())
         bot.send_message(message.from_user.id, "А между прочим, бакс сейчас стоит " + coincheck())
+        bot.send_message(message.from_user.id, "Если задумал заправить свой сраный трактор и свалить из сраной рашки, то бензинчик сейчас стоит " + petrolcheck())
         # проверяем ответ на привет
         print("Отклик на привет")
     elif message.text == "/help":
